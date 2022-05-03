da17d3f3867e44a5777fe454f66cc16c1267ff1a
Sarah Adewale <saraphynaadewale@gmail.com>
2022-05-03 23:45:21 +0100

    started javascript

--- api-template/js/main.js
+++ api-template/js/main.js
@@ -1,22 +1,39 @@
-//Example fetch using pokemonapi.co
-// document.querySelector('button').addEventListener('click', getFetch)
 
-// function getFetch(){
-//   const choice = document.querySelector('input').value
-//   console.log(choice)
+document.querySelector('.btn').addEventListener('click', getFetch)
 
-//   const url = `https://rickandmortyapi.com/api/character/?name=${choice}`
+function getFetch(){
+  const choice = document.querySelector('input').value
+  console.log(choice)
 
-//   fetch(url)
-//       .then(res => res.json()) // parse response as JSON
-//       .then(data => {
-//         console.log(data.results)
+  const url = `https://rickandmortyapi.com/api/character/?name=${choice}`
+//   const xterObj = data.results[0]
+
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data.results)
+        const xterObj = data.results[0]
+        // const episodes = xterEpisodes(data.results[0].episodes)
         
-//         document.querySelector('img').src = data.results[0].image
-//         document.querySelector('h3').innerText = data.results[0].name
-//       })
-//       .catch(err => {
-//           console.log(`error ${err}`)
-//       });
-// }
+        document.querySelector('#rick-image').src = xterObj.image
+        document.querySelector('.character-name').innerText = xterObj.name
+        document.querySelector('.character-species').innerText = xterObj.species
+        // document.querySelector('#character-species').innerText = xterObj.species
+        document.querySelector('.location-title').innerText = xterObj.location.name
+        // const episodes = xterEpisodes(data.results[0].episodes)
+        // function xterEpisodes(arr){
+        //     arr.forEach(element => {
+        //         element
+        //         console.log(element)
+        //     });
+        //     return document.querySelector('.character-episode-name').innerText = episodes
+            
+        // }
+        // document.querySelector('.character-episode-name').innerText = xterObj.
+
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+}
 
